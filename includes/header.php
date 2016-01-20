134337173624b02b7f4fb344cb44f3da08dfbb30
zmartl-bbc <luca.marti@bluewin.ch>
2016-01-20 19:58:18 +0100

    Design index
    Signed-off-by: zmartl-bbc <luca.marti@bluewin.ch>

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,33 @@
 <header>
-		<div id="banner">
-			<img alt="banner" src="images/banner/test.jpg">
+	<nav>
+		<div id="navigation">
+			<div class="navigationItem">
+				<p>Praxis Witzig</p>
+			</div>
+			<div class="navigationItem selected">
+				<a href="index.php">Home</a>
+			</div>
+			<div class="navigationItem">
+				<a href="#">Angebot</a>
+			</div>
+			<div class="navigationItem">
+				<a href="#">Lageplan</a>
+			</div>
+			<div class="navigationItem">
+				<a href="team.php">Team</a>
+			</div>
+			<div class="navigationItem">
+				<a href="#">Link</a>
+			</div>
+			<div class="navigationItem">
+				<a href="#">&Ouml;ffnungszeiten</a>
+			</div>
 		</div>
-		<nav>
-			<ul>
-				<li>
-					<p>Praxis Witzig</p>
-				</li>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="#">Angebot</a></li>
-				<li><a href="#">Lageplan</a></li>
-				<li><a href="team.php">Team</a></li>
-				<li><a href="#">Link</a></li>
-				<li><a href="#">&Ouml;ffnungszeiten</a></li>
-			</ul>
-		</nav>
-	</header>
+		<div class="clear"></div>
+	</nav>
+	<div class="select"></div>
+
+	<div id="banner">
+		<img alt="banner" src="images/banner/test.jpg">
+	</div>
+</header>
--- index.php
+++ index.php
@@ -7,44 +7,13 @@ include_once 'includes/head.php';
 	<?php
 	include_once 'includes/header.php';
 	?>
-	<div id="container">
-		<div class="articleround">
-			<div class="articlespecial">
-				<div class="articleimg">
-					<img alt="round img" src="images/article/one.gif">
-				</div>
-				<h2>Team</h2>
-				<div class="articlep">
-					<p>Unser kompetentes und motiviertes Team ber&auml;t Sie gerne.
-						Hier k&ouml;nnen Sie sehen, wer zu diesem z&auml;hlt.</p>
-					<a class="button" href="team.php">Mehr..</a>
-				</div>
-			</div>
-			<div class="articlespecial">
-				<div class="articleimg">
-					<img alt="round img" src="images/article/one.gif">
-				</div>
-				<h2>Anfahrt</h2>
-				<div class="articlep">
-					<p>Direkt unter dem Spital Uster, 10 min. vom Bahnhof. Reservierte
-						Parkpl&auml;tze sind vorhanden.</p>
-					<a class="button" href="#">Mehr..</a>
-				</div>
-			</div>
-			<div class="articlespecial">
-				<div class="articleimg">
-					<img alt="round img" src="images/article/one.gif">
-				</div>
-				<h2>Angebot</h2>
-				<div class="articlep">
-					<p>Hier bekommen Sie eine &Uuml;bersicht &uuml;ber all unsere
-						Angebote.</p>
-					<a class="button" href="#">Mehr..</a>
-				</div>
-			</div>
-		</div>
-		<div class="float"></div>
+		<div class="bannerSide">
+		<article>
+			<h2>&Uuml;berschrift</h2>
+			<p>Beitrag sdfjahsdljfhsdjfhsdjfhsdjlfsdhjkfn</p>
+		</article>
 	</div>
+	<div class="clear"></div>
 	<?php
 	include_once 'includes/footer.php';
 	?>
