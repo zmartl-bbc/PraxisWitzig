fb88dd47ddedffbcc29f498cfb6af986de546d12
zmartl-bbc <luca.marti@bluewin.ch>
2016-01-20 18:45:32 +0100

    Signed-off-by: zmartl-bbc <luca.marti@bluewin.ch>

---
+++ includes/head.php
@@ -0,0 +1,19 @@
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="styles/app.css" type="text/css" />
+<link rel="shortcut icon" href="images/favoicon/favicon.ico"
+	type="image/x-icon" />
+<!-- Text einfüllen -->
+<meta name="description" content="" />
+<meta name="geo.position" content="47.3526412;8.722885799999972" />
+<meta name="geo.region" content="CH-ZRH" />
+<meta name="geo.placename" content="Zuerich" />
+<meta name="ICBM" content="47.3526412, 8.722885799999972" />
+<meta name="keywords"
+	content="Praxis Witzig, Dr. med. Dieter Witzig, Dieter Witzig, Witzig, Dieter, Artz, Uster" />
+<link href='https://fonts.googleapis.com/css?family=Open+Sans'
+	rel='stylesheet' type='text/css'>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<title>Praxis Witzig</title>
+</head>
