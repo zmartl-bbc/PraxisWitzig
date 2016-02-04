a70869de709570e57b1d0a7b3c118f8f239080ef
zmartl-bbc <luca.marti@bluewin.ch>
2016-02-04 22:06:26 +0100

    Signed-off-by: zmartl-bbc <luca.marti@bluewin.ch>

---
+++ includes/navigation.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	<div class="container">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php">Dr. med. Dieter Witzig</a>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse"
+			id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="offer.php">Angebot</a></li>
+				<li><a href="map.php">Lageplan</a></li>
+				<li><a href="team.php">Team</a></li>
+				<li><a href="links.php">Links</a></li>
+				<li><a href="open.php">&Ouml;ffnungszeiten</a></li>
+			</ul>
+		</div>
+		<!-- /.navbar-collapse -->
+	</div>
+	<!-- /.container -->
+</nav>
--- index.php
+++ index.php
@@ -1,21 +1,275 @@
 <!DOCTYPE html>
 <html lang="de">
-<?php
-include_once 'includes/head.php';
-?>
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="Luca Marti">
+
+<title>Praxis Witzig</title>
+
+<!-- Bootstrap Core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/modern-business.css" rel="stylesheet">
+<!-- Custom CSS -->
+<link href="css/custom.css" rel="stylesheet">
+<!-- Custom Fonts -->
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet"
+	type="text/css">
+</head>
+
 <body>
-	<?php
-	include_once 'includes/header.php';
-	?>
-		<div class="bannerSide">
-		<article>
-			<h2>&Uuml;berschrift</h2>
-			<p>Beitrag sdfjahsdljfhsdjfhsdjfhsdjlfsdhjkfn</p>
-		</article>
+	
+	<?php require_once 'includes/navigation.php'; ?>
+
+	<!-- Header Carousel -->
+	<header id="myCarousel" class="carousel slide">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
+			<div class="item active">
+				<div class="fill"
+					style="background-image: url('images/banner/test.jpg');"></div>
+				<div class="carousel-caption">
+					<h2>Familienplanung oder Schwangerschaftsverh&uuml;tung</h2>
+				</div>
+			</div>
+			<div class="item">
+				<div class="fill"
+					style="background-image: url('http://placehold.it/1900x1080&amp;text=Slide Two');"></div>
+				<div class="carousel-caption">
+					<h2>Ultraschall</h2>
+				</div>
+			</div>
+			<div class="item">
+				<div class="fill"
+					style="background-image: url('http://placehold.it/1900x1080&amp;text=Slide Three');"></div>
+				<div class="carousel-caption">
+					<h2>Beratung nach der Geburt</h2>
+				</div>
+			</div>
+		</div>
+
+		<!-- Controls -->
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			<span class="icon-prev"></span>
+		</a> <a class="right carousel-control" href="#myCarousel"
+			data-slide="next"> <span class="icon-next"></span>
+		</a>
+	</header>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12">
+				<h1 class="page-header">Facharzt FMH f&uuml;r Gyn&auml;kologie und
+					Geburtshilfe</h1>
+			</div>
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>
+							<i class="fa fa-map-marker"></i> So finden Sie uns
+						</h4>
+					</div>
+					<div class="panel-body">
+						<p>Die Arztpraxis liegt an der Brunnenstrasse 38 in Uster. Direkt
+							unter dem Spital Uster. Reservierte Parkpl$tze sind auf dem
+							ehemaligen Areal des Restaurants Schibli.</p>
+						<a href="map.php" class="btn btn-default">Zum Lageplan</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="panel panel-default">
+					<div class="panel-heading">
+						<h4>
+							<i class="fa fa-users"></i> Unser Team
+						</h4>
+					</div>
+					<div class="panel-body">
+						<p>Unser fachlich ausgebildetes und motiviertes Team freut sich,
+							Sie bald in der Praxis von Dr. med. Dieter Witzig begr&uuml;ssen
+							zu d&uuml;rfen. Hier gelangen Sie zur Mitarbeiter&uuml;bersicht.</p>
+						<a href="team.php" class="btn btn-default">Zur
+							Mitarbeiter&uuml;bersicht</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /.row -->
+		<!-- Features Section -->
+		<div class="row">
+			<div class="col-lg-12">
+				<h2 class="page-header">Konakt</h2>
+			</div>
+			<div class="col-md-6">
+				<h3>
+					<i class="fa fa-phone"></i> Telefon:
+				</h3>
+				<div class="col-md-12">
+					<h2>
+						<small>Montag, Dienstag, Mittwoch</small>
+					</h2>
+				</div>
+				<div class="col-md-6">
+					<p>
+						<strong>Morgens</strong>
+					</p>
+					<p>
+				
+				</div>
+				<div class="col-md-6">
+					<p>08:00 - 12:00 h</p>
+
+				</div>
+				<div class="col-md-6">
+					<p>
+						<strong>Nachmittags</strong>
+					</p>
+					<p>
+				
+				</div>
+				<div class="col-md-6">
+					<p>14:00 - 18:00 h</p>
+
+				</div>
+				<div class="col-md-12">
+					<h2>
+						<small>Donnerstag</small>
+					</h2>
+				</div>
+				<div class="col-md-6">
+					<p>
+						<strong>Morgens</strong>
+					</p>
+					<p>
+				
+				</div>
+				<div class="col-md-6">
+					<p>08:00 - 12:00 h</p>
+
+				</div>
+				<div class="col-md-6">
+					<p>
+						<strong>Nachmittags</strong>
+					</p>
+					<p>
+				
+				</div>
+				<div class="col-md-6">
+					<p>geschlossen</p>
+
+				</div>
+				<div class="col-md-12">
+					<h2>
+						<small>Freitag</small>
+					</h2>
+				</div>
+				<div class="col-md-6">
+					<p>
+						<strong>Morgens</strong>
+					</p>
+					<p>
+				
+				</div>
+				<div class="col-md-6">
+					<p>07:00 - 12:00 h</p>
+
+				</div>
+				<div class="col-md-6">
+					<p>
+						<strong>Nachmittags</strong>
+					</p>
+					<p>
+				
+				</div>
+				<div class="col-md-6">
+					<p>14:00 - 16:00 h</p>
+
+				</div>
+			</div>
+			<div class="col-md-6">
+				<img class="img-responsive" src="http://placehold.it/700x450" alt="">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<h3>Schicken Sie uns eine Nachricht</h3>
+				<form name="sentMessage" id="contactForm" novalidate>
+					<div class="control-group form-group">
+						<div class="controls">
+							<label>Vor- und Nachname: *</label> <input type="text"
+								class="form-control" id="name" required
+								data-validation-required-message="Bitte den Vor- und Nachnamen eingeben.">
+							<p class="help-block"></p>
+						</div>
+					</div>
+					<div class="control-group form-group">
+						<div class="controls">
+							<label>Telefonnummer:</label> <input type="tel"
+								class="form-control" id="phone">
+						</div>
+					</div>
+					<div class="control-group form-group">
+						<div class="controls">
+							<label>Email: *</label> <input type="email" class="form-control"
+								id="email" required
+								data-validation-required-message="Bitte Email Adresse eingeben.">
+						</div>
+					</div>
+					<div class="control-group form-group">
+						<div class="controls">
+							<label>Nachricht: <small>(max 1500 Zeichen)</small> *
+							</label>
+							<textarea rows="10" cols="100" class="form-control" id="message"
+								required
+								data-validation-required-message="Bitte eine Nachricht eingeben."
+								maxlength="1500" style="resize: none"></textarea>
+						</div>
+					</div>
+					<p>Mit * markierte Felder sind Pflichtfelder.</p>
+					<div id="success"></div>
+					<!-- For success/fail messages -->
+
+					<button type="submit" class="btn btn-primary">Nachricht senden</button>
+				</form>
+			</div>
+
+		</div>
+		<!-- /.row -->
+		<!-- Footer -->
+		<footer>
+			<div class="row">
+				<div class="col-lg-12">
+					<p>Copyright &copy; Your Website 2014</p>
+				</div>
+			</div>
+		</footer>
+
 	</div>
-	<div class="clear"></div>
-	<?php
-	include_once 'includes/footer.php';
-	?>
+	<!-- /.container -->
+
+	<!-- jQuery -->
+	<script src="js/jquery.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/contact_me.js"></script>
+	<script src="js/jqBootstrapValidation.js"></script>
+	<!-- Script to Activate the Carousel -->
+	<script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+
 </body>
+
 </html>
